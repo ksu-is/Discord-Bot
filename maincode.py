6927e9fded6c3b5a1ec48873e298947223e9f93b
sholla43 <97647832+sholla43@users.noreply.github.com>
2022-04-22 13:39:38 -0400

    Create maincode.py

---
+++ maincode.py
@@ -0,0 +1,22 @@
+#Discord Bot from Python code              by: Seth H
+
+import discord
+import random
+
+TOKEN = "YOUR_TOKEN_GOES_HERE"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("{0.user} is now online!".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello there!')
+
+client.run(TOKEN)
