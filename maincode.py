505f3266f5750cdfbdd52d1d6be8517de5aeac5a
Seth Holland <sholla43@students.kennesaw.edu>
2022-05-07 17:23:59 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,9 +1,17 @@
 import discord
 import random
+import requests
+import json
 
-TOKEN = 'OTcyMjExMjU4NTY2Nzc4ODkw.YnVwSg.7P5SZSGHI4tjS4nyPUr33Brer7Y'
+TOKEN = 'YOUR_TOKEN_GOES_HERE'
 
 client = discord.Client()
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data [0] ['q'] + " -" + json_data [0] ['a']
+    return(quote)
 
 @client.event
 async def on_ready():
@@ -26,10 +34,22 @@ async def on_message(message):
         elif user_message.lower() == 'bye':
             await message.channel.send(f'See you later {username}!')
             return
+        elif user_message.lower() == '$sports':
+            await message.channel.send(f'https://ksuowls.com/calendar')
+            return
         elif user_message.lower() == '!random':
             response = f'This is your random number: {random.randrange(1000000)}'
             await message.channel.send(response)
             return
+        elif user_message.lower() == '$joke':
+            await message.channel.send(f'Owls are clearly smarter than chickens... you’ve never heard of Kentucky-fried owl!')
+            return
+        elif user_message.lower() == '$joke2':
+            await message.channel.send(f'What is most common form of violence amongst owls? Fly-by hooting!')
+            return
+        elif user_message.lower() == '$inspire':
+            quote = get_quote()
+            await message.channel.send(quote)
 
 client.run(TOKEN)
 
